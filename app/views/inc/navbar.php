7d322b14077310f69f0bc4a0e39fd444ff6711d0
John Silver <tienduy0123@gmail.com>
2023-03-25 21:50:01 +0100

    Add nav-bar

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<div>
+    <div class="nav-bar-element">
+        <a href="#" class="element-title">Docter</a>
+    </div>
+    <div class="nav-bar-element">
+        <a href="#" class="element-title">Patient</a>
+    </div>
+    <div class="nav-bar-element">
+        <a href="#" class="element-title">Login</a>
+    </div>
+    <div class="nav-bar-element">
+        <a href="#" class="element-title">About</a>
+    </div>
+    <div class="nav-bar-element">
+        <a href="#" class="element-title">HOME</a>
+    </div>
+</div>
+</html>
